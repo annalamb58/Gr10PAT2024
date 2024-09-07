1950e3a6edcde18e7974df99ef7465a497a28add
HYSTOU <HYSTOU@192.168.5.165>
2024-09-07 08:13:00 +0200

    My first commit All names and structure done for UI. Everything is changed to main colour grey (217,217,217). Start of delete Species list code.

---
+++ src/main/java/Frontend/Gr10_PAT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Frontend;
+
+/**
+ *
+ * @author HYSTOU
+ */
+public class Gr10_PAT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

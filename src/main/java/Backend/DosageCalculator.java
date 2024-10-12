8aee7d9a933db700255d0febb8829dcacd6ae981
HYSTOU <HYSTOU@192.168.5.165>
2024-10-12 20:40:21 +0200

    Backend Updated -Dosage Calcutlator Started code with the help of chatGPT for ideas and a few lines of code Did the (OLD CODE #) and put it through chatGPT commented (OLD CODE #) and made a section to break up (OLD CODE #) and "new code" wrote the new code from coping old code and then making changing to go with what chatGPT wrote What this code does: breaks each like in the textfile into tokens checks for the (each token) line in the textfile to what the user entered. (need to change a few things to do with the formula)

--- src/main/java/Backend/DosageCalculator.java
+++ src/main/java/Backend/DosageCalculator.java
@@ -20,31 +20,116 @@ public class DosageCalculator {
         // NOTE ORDER IN DOC = meds + " " + species + " " + formula + " " + dose;
         File fileForCAL = new File("PAT//meds/text");
         Scanner scannerForCAL = new Scanner(fileForCAL);
-        
+
+        // To save tokens
         String currentMeds = "";
         String currentSpecies = "";
         String currentFormula = "";
         String currentDose = "";
-        
-        boolean isMed = false;
-        boolean isSpecies = false;
-        boolean isFormula = false;
-        boolean isDose = false;
-        
-        // Getting correct medication's name
-        while (scannerForCAL.hasNext()) {
-              currentMeds = scannerForCAL.next();
-          if (currentMeds == meds) {
-              isMed = true ;
-          } else {
-              currentMeds =scannerForCAL.next();
-          } // end of Meds if statement
-        } // end of Meds while loop
-        
-        
-        
-        
-        
+
+        // To find line
+        boolean isMatch = false;
+
+        // formula
+        String formulaWeight = "";
+        String formulaMultiply = "*";
+        String formulaMg_kg = "";
+        String formulaDivide = "/";
+        String formulaMg = "";
+
+        while (scannerForCAL.hasNextLine()) {
+            String line = scannerForCAL.nextLine();
+
+            //Breaking line into tokens
+            String[] tokens = line.split(" ");
+
+            if (tokens.length >= 4) {
+                // Saving the tokens to variables
+                currentMeds = tokens[0];
+                currentSpecies = tokens[1];
+                currentFormula = tokens[2];
+                currentDose = tokens[3];
+
+                // Check for a match
+                if (currentMeds.equalsIgnoreCase(meds)
+                        && currentSpecies.equalsIgnoreCase(species)
+                        && currentFormula.equalsIgnoreCase(formula)
+                        && currentDose.equalsIgnoreCase(dose)) {
+                    isMatch = true; // Set matchFound to true
+                    break; // Exit the loop if a match is found
+                } // End of if current == entered statemeny
+            } // end of Token if statement
+
+            if (isMatch) {
+              
+            }
+       
+
+        } // end of while scannerForCAL.hasNextLine() loop
     } // End of method CalculatingDose
 } // end of class
 
+// OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE # OLD CODE #
+//        String currentMeds = "";
+//        String currentSpecies = "";
+//        String currentFormula = "";
+////        String currentDose = "";
+//
+//        boolean isMed = false;
+//        boolean isSpecies = false;
+//        boolean isFormula = false;
+//        boolean isDose = false;
+//        boolean isLine = false;
+//
+//        while (scannerForCAL.hasNextLine()) {
+//            String line = scannerForCAL.nextLine();
+//
+//            //Breaking line into tokens
+//            String[] tokens = line.split(" ");
+//
+//            if (tokens.length >= 4) {
+//                // Saving the tokens to variables
+//                currentMeds = tokens[0];
+//                currentSpecies = tokens[1];
+//                currentFormula = tokens[2];
+//                currentDose = tokens[3];
+//            } // end of Token if statement
+//
+//            //checking if med name and what was entered match
+//            if (currentMeds.equalsIgnoreCase(meds)) {
+//                isMed = true;
+//            } else {
+//                line = scannerForCAL.nextLine();
+//            } // end of check Meds if statement
+//
+//            // if med name is correct:
+//            //checking if species and what was entered match
+//            if (isMed = true) {
+//                if (currentSpecies.equalsIgnoreCase(species)) {
+//                    isSpecies = true;
+//                } else {
+//                    line = scannerForCAL.nextLine();
+//                } // end of check species = true if statement
+//            }// end of isMed = true if statement
+//
+//            // if med name and species is correct:
+//            //checking if formula and what was entered match
+//            if (isMed == true && isSpecies == true) {
+//                if (currentFormula.equalsIgnoreCase(formula)) {
+//                    isFormula = true;
+//                } else {
+//                    line = scannerForCAL.nextLine();
+//                }// end of check formula = true if statement
+//            }// end of isMed && isSpecies = true if statement
+//
+//            // if med name and species and formula is correct:
+//            //checking if dose and what was entered match
+//            if (isMed == true && isSpecies == true && isFormula == true) {
+//                if (currentDose.equalsIgnoreCase(dose)) {
+//                    isDose = true;
+//                } else {
+//                    line = scannerForCAL.nextLine();
+//                }// end of check dose = true if statement
+//            }// end of isMed && isSpecies && isFormula = true if statement 
+//
+//        } // end of while hasNextLine loop
